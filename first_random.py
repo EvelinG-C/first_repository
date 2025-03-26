934c28e65c9a385bf5f7a3c8e27bd4428a112536
Evelin Garcia <eve2177109@maricopa.edu>
2025-03-25 19:35:17 -0700

    First commit.

---
+++ first_random.py
@@ -0,0 +1,13 @@
+"""
+Evelin Garcia Contreras
+Module 08 Lab Assignment
+Part B
+
+This is a random module
+that does not have a big
+purpose.
+"""
+
+user_input = input("What is your favorite color?")
+
+print("Your favorite color is:", user_input)
